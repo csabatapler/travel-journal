98343cc0b9d0b9fc54a6fbdc938c28f37e91f7ba
Csaba Tapler <164663981+csabatapler@users.noreply.github.com>
2024-06-14 11:38:40 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import './App.css'
+import React from 'react'
+import ReactDOM from "react-dom"
+import data from "../public/data.js"
+import Card from "./Card.jsx"
+
+
 
 function App() {
+  const cardList = data.map((item,index) => {
+  return(
+    <Card
+      key={index}
+      item = {item}
+    />
+  )    
+  })
 
+  console.log({cardList})
 
   return (
-    <>
-
-    </>
+    <div className='cardList'>
+      {cardList}
+  
+    </div>
   )
 }
 
 export default App
+
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props)
   return (
   <div className='card'>
-
-      <img src="https://source.unsplash.com/WLxQvbMyfas" alt="image-of-a-landscape" className='imageUrl'/>
-      <div className='card--info'>
+      <img src={props.item.imageUrl} alt="image-of-a-landscape" className='card-image'/>
+      <div className='card-info'>
         <div className='card-location-container'>
           <img src="../public/images/pin.svg" alt="location pin"></img>
-          <p className='location'>Country</p>
-          <p className="google-map"></p>
-          <h1 className='title'>Mount Fuji</h1>
-          <h3 className='duration'>12 Jan, 2021 - 24 Jan, 2021</h3> 
-          <p className='description'></p>
-
+          <p className='location'>{props.item.location}</p>
+          <a className="google-map" href={props.item.googleMapsUrl}>View on Google Maps</a>
+        </div>
+        <div className="card-details-container">
+          <h1 className='title'>{props.item.title}</h1>
+          <h3 className='duration'>{props.item.startDate}-{props.item.endDate}</h3> 
+          <p className='description'>{props.item.description}</p>
         </div>
       </div>
-
   </div>)
 }
